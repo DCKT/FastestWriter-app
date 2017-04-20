16c40f1320815a94807502b0c3d830e261d1ab36
Thomas Deconinck <thomas.d@colisweb.com>
2017-04-20 13:29:32 +0200

    Add mobile-center-crashes

--- android/app/src/main/java/com/projectname/MainApplication.java
+++ android/app/src/main/java/com/projectname/MainApplication.java
@@ -3,6 +3,7 @@ package com.projectname;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
 import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.cboy.rn.splashscreen.SplashScreenReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
             new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
             new LinearGradientPackage(),
             new SplashScreenReactPackage()
